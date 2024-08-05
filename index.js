ce7e5909b97fefa6d5b6d255a1cd56d0a78e641c
kakadiayaprince <156084541+kakadiayaprince@users.noreply.github.com>
2024-08-05 11:10:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,18 +14,39 @@ arr = [{
     id: 3
 }
 ]
-app.all('/home',(req,res)=>{
-res.send('oops! please give id ')
+app.all('/home', (req, res) => {
+    res.send('oops! please give id ')
 })
 app.get('/home/:id', (req, res) => {
-   const sid = req.params.id
-   const ans = arr.find(e=>e.id==sid)
-   if(ans){
-    res.json(ans)
-   }
-   else{
-    res.write("not found data:")
-   }
-}).listen(3000, (req, res) => {
+    const sid = req.params.id
+    const ans = arr.find(e => e.id == sid)
+    if (ans) {
+        res.json(ans)
+    }
+    else {
+        res.write("not found data:")
+    }
+})
+
+const bodyparser = require('body-parser')
+app.use(bodyparser.urlencoded({
+    extended: false
+}))
+
+//post method:
+
+app.post('/home/add', (req, res) => {
+    const { id, name } = req.body;
+    const ans = {
+        'name': name,
+        'id': id
+    }
+    arr.push(ans)
+    res.json(arr)
+})
+
+
+
+app.listen(3000, (req, res) => {
     console.log("app is running at 3000:")
 })
